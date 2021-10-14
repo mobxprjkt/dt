275668b043b2ee0c07bceaed6fef3baec01086bf
Muhammad Fadly A S <mhmmdfdlyas@gmail.com>
2021-10-14 08:42:54 +0700

    juice: define DT2W node
    Change-Id: I5bf351c77201475a0130c32b46807d9eeb41ddac

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,9 @@ HWUI_COMPILE_FOR_PERF := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
